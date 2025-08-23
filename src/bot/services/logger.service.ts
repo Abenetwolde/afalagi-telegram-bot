6eb8512732916b820c5146c6d06b064573a7126b
Abenetwolde <abnetwoldedev@gmail.com>
2025-08-23 15:04:21 +0300

    fix logger

--- src/bot/services/logger.service.ts
+++ src/bot/services/logger.service.ts
@@ -4,11 +4,9 @@ export const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
     winston.format.timestamp(),
-    winston.format.json(),
+    winston.format.json()
   ),
   transports: [
-    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
-    new winston.transports.File({ filename: "logs/combined.log" }),
-    new winston.transports.Console(),
-  ],
+    new winston.transports.Console() // Logs to stdout, captured by CloudWatch
+  ]
 });
